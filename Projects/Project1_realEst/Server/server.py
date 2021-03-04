7071ab0331c5531ad08c4e0c8ecc47c59fbbd061
Naman Solanki <namansolanki549@gmail.com>
2021-03-05 03:54:31 +0530

    Add files via upload

---
+++ Projects/Project1_realEst/Server/server.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    size = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+    bal = float(request.form['balconies'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft, bath, bal, size)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
+
+
+
+
+
+# from flask import Flask, request, jsonify
+# import util
+# app = Flask(__name__)
+#
+# @app.route('/get_location_names', methods=['GET'])
+# def get_location_names():
+#     response = jsonify({
+#         'locations': util.get_location_names()
+#     })
+#     print(get_location_names())
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     return response
+#
+# @app.route('/predict_home_price', methods=['GET', 'POST'])
+# def predict_home_price():
+#     total_sqft = float(request.form['total_sqft'])
+#     location = request.form['location']
+#     size = int(request.form['Size'])
+#     bath = int(request.form['bath'])
+#     bal = float(request.form['balconies'])
+#
+#     response = jsonify({
+#         'estimated_price': util.get_estimated_price(location, sqft, bath, bal, size)
+#     })
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#
+#     return response
+#
+# if __name__ == "__main__":
+#     print('Starting Flask server for python price prediction...')
+#     util.load_saved_artifacts()
+#     app.run()
